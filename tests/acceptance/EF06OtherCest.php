b97550b1091db2d32e02d8b45e857c0e33936e09
Kentaro Ohkouchi <nanasess@fsm.ne.jp>
2016-11-10 10:35:27 +0900

    EF06のみ実行

--- tests/acceptance/EF06OtherCest.php
+++ tests/acceptance/EF06OtherCest.php
@@ -59,9 +59,6 @@ class EF06OtherCest
         $I->see('ログインできませんでした。', '#login_mypage .text-danger');
     }
 
-    /**
-     * @group mail
-     */
     public function other_パスワード再発行(\AcceptanceTester $I)
     {
         $I->wantTo('EF0602-UC01-T01 パスワード再発行');
